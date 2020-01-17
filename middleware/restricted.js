eae57927d8b776def482c45993e9a0ff3268b4b8
spettigrew <sara.pettigrew5@gmail.com>
2020-01-16 19:07:01 -0700

    Middleware modified

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,36 +1,10 @@
-const bcrypt = require("bcryptjs")
-const usersModel = require("../users/users-model")
+
 
 module.exports = () => {
-  // put message in a variable so we can reuse it
-  const authError = {
-    message: "Invalid credentials",
+  return (req, res, next) => {
+  if (req.session && req.session.user) {
+   return res.status(401).json({ message: "Incorrect credentials provided." })
   }
-
-  return async (req, res, next) => {
-    try {
-      const { username, password } = req.headers
-      // make sure the values aren't empty
-      if (!username || !password) {
-        return res.status(401).json(authError)
-      }
-
-      const user = await usersModel.findBy({ username }).first()
-      // make sure user exists in the database
-      if (!user) {
-        return res.status(401).json(authError)
-      }
-
-      const passwordValid = await bcrypt.compare(password, user.password)
-      // make sure password is correct
-      if (!passwordValid) {
-        return res.status(401).json(authError)
-      }
-
-      // if we reach this point, the user is authenticated!
-      next()
-    } catch (err) {
-      next(err)
-    }
+  next()
   }
 }
