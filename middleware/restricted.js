d0d4880500ab0e697c5775da8782074a2251aae4
spettigrew <sara.pettigrew5@gmail.com>
2020-01-16 19:57:26 -0700

    "Not" added to 'if' statement

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -2,7 +2,7 @@
 
 module.exports = () => {
   return (req, res, next) => {
-  if (req.session && req.session.user) {
+  if (!req.session && !req.session.user) {
    return res.status(401).json({ message: "Incorrect credentials provided." })
   }
   next()
